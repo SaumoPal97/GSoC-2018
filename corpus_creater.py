09d6ba31c0d797679fa6b90743a352216a4d4c3a
SaumoPal97 <saumopal1997@gmail.com>
2018-05-29 18:00:03 +0530

    Changed variables

--- corpus_creater.py
+++ corpus_creater.py
@@ -19,9 +19,10 @@ def deep_search(needles, haystack, found) :
                                 found.append(ancestor)
                         elif needle == 'resource-id' :
                             m = re.search('(?<=:id/)\w+', haystack[needle])
-                            words = m.group(0).split('_')
-                            for word in words :
-                                found.append(word)
+                            if m is not None :
+                            	words = m.group(0).split('_')
+                            	for word in words :
+                                	found.append(word)
                         elif needle == 'long-clickable' and haystack[needle] == True :
                             found.append(needle) 
                         elif needle != 'long-clickable' and len(haystack[needle]) > 0 :
@@ -39,24 +40,24 @@ def deep_search(needles, haystack, found) :
 def main() :
     mypath = getcwd()
     files = [f for f in listdir(join(mypath,'json_dir_1')) if isfile(join(join(mypath,'json_dir_1'), f))]
-    for file in files :
-        with open(join(join(mypath,'json_dir_1'),file),'r') as f:
+    for fl in files :
+    	print fl
+        with open(join(join(mypath,'json_dir_1'),fl),'r') as f:
             fo = json.load(f)
         found = list()
         needles = [u'text',u'long-clickable',u'text-hint',u'bounds',u'font-family',u'ancestors',u'resource-id']
         deep_search(needles, fo, found)
         f.close()
-            
-        fname = splitext(file)[0] + '.txt'
+    
+        fname = splitext(fl)[0] + '.txt'
         fi = open(join(join(mypath,'corpus_dir_1'), fname), 'w+')
         for word in found :
             if word != (' ') :
-                fi.write(repr(word).encode('utf-8'))
+            	fi.write(repr(word).encode('utf-8'))
                 fi.write(' ')
             elif word == (' ') :
-                fi.write('\n')
+               	fi.write('\n')
         fi.close()
-
 
 if __name__ == "__main__":
     main()
