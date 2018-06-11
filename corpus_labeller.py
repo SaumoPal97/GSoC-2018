46d584575e379e588c45872eb58d0ddc5260723a
SaumoPal97 <saumopal1997@iitkgp.ac.in>
2018-06-11 12:00:49 +0530

    Added corpus labelling code

---
+++ corpus_labeller.py
@@ -0,0 +1,55 @@
+import os, json
+from collections import OrderedDict
+
+label_dict = OrderedDict()
+label_dict['email'] = [u'email']
+label_dict['text'] = [u'name',u'address',u'first',u'last',u'street',u'building']
+label_dict['password'] = [u'password']
+label_dict['number'] = [u'phone',u'mobile',u'pincode',u'zipcode',u'pin',u'zip']
+label_dict['ad'] = [u'adview', u'advert',u'advertisement']
+label_dict['skip'] = [u'skip',u'close',u'stop']
+label_dict['image'] = [u'thumbnail',u'image',u'clip',u'imageview']
+label_dict['button'] = [u'button']
+
+def jsonify_data(top_directory):
+    json_list = list()
+    for root, dirs, files in os.walk(top_directory):
+        for file in filter(lambda file: file.endswith('.txt'), files):
+            with open(os.path.join(root, file)) as f:
+                document = f.readlines()
+                f.close()
+            document = [line.strip() for line in document]
+            for line in document:
+                json_dict = {}
+                for key, value in label_dict.iteritems():
+                    #print key
+                    #print value
+                    #print line
+                    if any(x in line for x in value):
+                        if (key in ['email','text','number','password']):
+                            if(u'edittext' in line):
+                                json_dict['feature'] = line
+                                json_dict['type'] = key
+                                json_list.append(json_dict)
+                                break
+                            else :
+                                json_dict['feature'] = line
+                                json_dict['type'] = 'button'
+                                json_list.append(json_dict)
+                                break
+                        else :
+                            json_dict['feature'] = line
+                            json_dict['type'] = key
+                            json_list.append(json_dict)
+                            break
+                    else:
+                        json_dict['feature'] = line
+                        json_dict['type'] = 'test'
+                        json_list.append(json_dict)
+    json_list = [dict(t) for t in set([tuple(d.items()) for d in json_list])]
+    thefile = open('json_corpus.txt', 'w+')
+    for item in json_list:
+        thefile.write("%s\n" % item)
+        
+top_directory = '/home/saumo/Desktop/GSoC-2018/corpus_dir_1/'
+jsonify_data(top_directory)
--- language_transformations.py
+++ language_transformations.py
@@ -21,21 +21,21 @@ def create_dictionary(top_directory):
 def transform_bow(corpus_list, dictionary, top_directory):
     corpus_bow = list()
     corpus_bow += [dictionary.doc2bow(c) for c in corpus_list]
-    gensim.corpora.MmCorpus.serialize(os.path.join(top_directory, 'trial.mm'), corpus_bow)
+    gensim.corpora.MmCorpus.serialize(os.path.join(top_directory,'..','trial.mm'), corpus_bow)
     return corpus_bow
 
 def transform_tfidf(corpus_bow, top_directory):
     tfidf = gensim.models.TfidfModel(corpus_bow)
-    tfidf.save(os.path.join(top_directory, 'trial.tfidf'))
+    tfidf.save(os.path.join(top_directory,'..','trial.tfidf'))
     corpus_tfidf = tfidf[corpus_bow]
     return corpus_tfidf
 
 def transform_lsi(corpus_tfidf, dictionary, top_directory):
     lsi = gensim.models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=50)
-    lsi.save(os.path.join(top_directory, 'trial.lsi'))
+    lsi.save(os.path.join(top_directory,'..','trial.lsi'))
     corpus_lsi = lsi[corpus_tfidf]
     index = gensim.similarities.MatrixSimilarity(corpus_lsi)
-    index.save(os.path.join(top_directory, 'trial.index'))
+    index.save(os.path.join(top_directory,'..','trial.index'))
     return index
   
 top_directory = '/home/saumo/Desktop/GSoC-2018/corpus_dir_1/'
