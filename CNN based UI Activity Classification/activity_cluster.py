5f5bc0eeaf38f291c6f3866096b75cab9b9ed3e1
SaumoPal97 <saumopal1997@iitkgp.ac.in>
2018-07-02 23:49:49 +0530

    Made vectorised function and reduced percentage of data used for clustering

--- CNN based UI Activity Classification/activity_cluster.py
+++ CNN based UI Activity Classification/activity_cluster.py
@@ -1,10 +1,18 @@
 import numpy as np
 import pandas as pd
 import sklearn.cluster
-import distance
+import distance      
+from random import shuffle
+
+def lev_similarity_func(w1,w2):
+    return distance.levenshtein(w1,w2)
 
 def activity_clusters(words):
-    lev_similarity = -1*np.array([[distance.levenshtein(w1,w2) for w1 in words] for w2 in words])
+    #unvectorized code
+    #lev_similarity = -1*np.array([[distance.levenshtein(w1,w2) for w1 in words] for w2 in words])
+    #vectorized code
+    vlev_similarity = np.vectorize(lev_similarity_func)
+    lev_similarity = -1*vlev_similarity(words,np.array([words]).T) 
     affprop = sklearn.cluster.AffinityPropagation(affinity="precomputed", damping=0.5)
     affprop.fit(lev_similarity)
     for cluster_id in np.unique(affprop.labels_):
@@ -15,7 +23,12 @@ def activity_clusters(words):
 
 def main():
     df = pd.read_csv('complete_activity_list.csv')
-    words = np.asarray(df['ActivityName'])
+    new_complete_list = list()
+    for d in df['ActivityName']:
+        if type(d) is str:
+            new_complete_list.append(d)
+    shuffle(new_complete_list)
+    words = np.asarray(new_complete_list[:int(0.02*len(new_complete_list))])
     activity_clusters(words)
 
 if __name__ == "__main__":
